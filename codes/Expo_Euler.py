d7d950ecac503c9ac0d61eda19479a2a938ee6ee
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2021-05-31 17:21:35 +0200

    Create Expo_Euler.py

---
+++ codes/Expo_Euler.py
@@ -0,0 +1,43 @@
+def func(x, y):
+    sum = x + y
+    return sum
+
+def Euler(x0, y0, xn, n):
+    a = (xn - x0) / n
+
+    print_str1 = '\n-----------------WYNIK----------------'
+    print_str2 = '---------------------------------------'
+    print_str3 = 'x0\ty0\tnachylenie\tyn'
+
+    print(print_str1)
+    print(print_str2)
+    print(print_str3)
+    print(print_str2)
+
+    for i in range (n):
+        nachylenie = func(x0, y0)
+        yn = y0 + a * nachylenie
+        print('%.2f\t %.2f\t %0.2f\t %.2f'% (x0, y0, nachylenie, yn))
+        print(print_str2)
+
+        y0 = yn
+        x0 = x0 + a
+
+    print('\nW x = %.2f, y = %.2f'% (xn, yn))
+
+def Euler_Exp():
+    enter_str1 = 'Wprowadz dane do obliczenia:\n'
+    enter_str2 = 'Wprowadz punkt kalkulacji:\n'
+    enter_str3 = 'Wprowadz liczbe krokow:\n'
+
+    print(enter_str1)
+    x0 = float ( input('Podaj x0: '))
+    y0 = float ( input('Podaj y0: '))
+
+    print(enter_str2)
+    xn = float ( input('Podaj xn: '))
+
+    print(enter_str3)
+    kroki = int ( input('Podaj liczbe krokow: '))
+
+    Euler(x0, y0, xn, kroki)
